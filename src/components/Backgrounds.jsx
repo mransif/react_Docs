203c49dce8501be0c99f25448026c0b7ddfdc93a
Kenz8489 <stark858981@gmail.com>
2025-02-22 21:36:41 +0530

    Updated features and fixed bugs

--- src/components/Backgrounds.jsx
+++ src/components/Backgrounds.jsx
@@ -4,7 +4,7 @@ const Backgrounds = () => {
   return (
     <div className='fixed z-[2] w-full h-screen'>
         <div className="absolute nav w-full py-5 text-zinc-500 text-xl flex justify-center">Documents</div>
-        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-900 text-[15vw] leading-none tracking-tighter'>Docs.</h1>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-cyan-900 text-[15vw] leading-none tracking-tighter'>Docs.</h1>
     </div>
   )
 }
